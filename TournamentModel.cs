82392a9f0e53370c57953f79234cade23f5c0960
IkezawaYuki <y.ikezawa93@gmail.com>
2022-04-21 23:25:32 +0900

    mod

--- TournamentModel.cs
+++ TournamentModel.cs
@@ -8,6 +8,10 @@ namespace TrackerLibrary
 {
     public class TournamentModel
     {
-
+        public string TournamentName { get; set; }
+        public decimal EntryFee { get; set; }
+        public List<TeamModel> EnteredTeams { get; set; } = new List<TeamModel>();
+        public List<PrizeModel> Prizes { get; set; } = new List<PrizeModel>();
+        public List<MatchupModel> Rounds { get; set; } = new List<MatchupModel>();
     }
 }
