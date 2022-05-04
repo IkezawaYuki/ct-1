bb02a5688e086467d3d77e2405b85183efc768be
IkezawaYuki <y.ikezawa93@gmail.com>
2022-05-04 23:34:46 +0900

    mod

---
+++ SqlConnector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class SqlConnector : IDataConnection
+    {
+        public PrizeModel CreatePrize(PrizeModel prize)
+        {
+            prize.Id = 1;
+
+            return prize;
+        }
+    }
+}
---
+++ TextConnection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    internal class TextConnection : IDataConnection
+    {
+        public PrizeModel CreatePrize(PrizeModel prize)
+        {
+            prize.Id = 1;
+
+            return prize;
+        }
+    }
+}
