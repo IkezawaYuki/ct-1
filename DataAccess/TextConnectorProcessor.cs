86a1f03c9ff6567fa270c561fe90f7635e9f8a3e
IkezawaYuki <y.ikezawa93@gmail.com>
2022-05-19 23:39:18 +0900

    mod

--- DataAccess/TextConnectorProcessor.cs
+++ DataAccess/TextConnectorProcessor.cs
@@ -46,7 +46,12 @@ namespace TrackerLibrary.DataAccess.TextHelpers
 
         public static void SaveToPrizeFile(this List<PrizeModel> models, string fileName)
         {
-
+             List<string> lines = new List<string>();
+            foreach(PrizeModel p in models)
+            {
+                lines.Add($"{p.Id},{p.PlaceNumber},{p.PlaceName},{p.PrizeAmount},{p.PrizePercentage}");
+            }
+            File.WriteAllLines(fileName.FullFilePath(), lines);
         }
     }
 }
