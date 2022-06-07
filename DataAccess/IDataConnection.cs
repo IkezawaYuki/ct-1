ca599d6dc50cbdd39a056db355504d07071b0af4
IkezawaYuki <y.ikezawa93@gmail.com>
2022-06-07 22:55:59 +0900

    mod

--- DataAccess/IDataConnection.cs
+++ DataAccess/IDataConnection.cs
@@ -12,6 +12,7 @@ namespace TrackerLibrary.DataAccess
         PrizeModel CreatePrize(PrizeModel prize);
         PersonModel CreatePerson(PersonModel person);
         TeamModel CreateTeam(TeamModel team);
+
         List<PersonModel> GetPerson_All();
     }
 }
